5353249ea5fa7e9b4f1bad9e6d0e3da6a6324d5e
ecloud <ecloudchow@gmail.com>
2016-12-25 18:13:20 +0800

    implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :only => [:new, :create]
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_paramss)
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_paramss
+    params.require(:post).permit(:content)
+  end
+  
 end
